3c826149c6b0e90da86e4b24e52e937ec3867e39
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2025-09-17 17:34:23 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reflection Questions
+
+**How did array methods like push() and filter() help you manipulate data in this activity?**
+ push() added new data in the end of array
+ filter() picked specific test implemented by a provided function
+
+**What challenges did you face when trying to filter and search for items in the array?**
+ I took time to find out diffrent from exact same test implemented by a provided function and it is included the speciic test implemented by a provided function.
+
+**How would you modify this code to make it more efficient or user-friendly?**
+ To append the new element to the ul is easier to show in the browser for user.
---
+++ lab4.js
@@ -0,0 +1,95 @@
+//Task1
+const shoppingList = [];
+function addItem(item){
+    let hasItem;
+    for(let product of shoppingList){
+        if(product==item){
+            hasItem=true;
+        }
+    }
+    if(!hasItem ){
+        shoppingList.push(item)
+    }
+    
+}
+//original
+//const addItem = (item)=>shoppingList.push(item);
+
+//const removeItem= ()=> shoppingList.pop();
+addItem('mouse')
+addItem('pc')
+addItem('monitor')
+addItem('keybord')
+addItem('pc')
+addItem('mac-pc')
+addItem('pcpc')
+addItem('putaropc')
+
+function removeItem(){
+    shoppingList.pop()
+}
+//removeItem()
+
+function displayList(){
+    console.log(shoppingList)
+ }
+displayList()
+
+//Task2
+
+function filterItems(searchTerm){
+    const result = shoppingList.filter((item) => item.includes(searchTerm))
+    console.log(result);
+}
+console.log(shoppingList);
+filterItems('pc')
+//Task3
+
+// 1.Create an HTML page with an input field, an “Add Item” button, and an unordered list to display the items.
+// 2.Write a JavaScript function that adds items to the array and updates the displayed list dynamically when the button is clicked.
+//const shoppingList = [];
+
+
+//select or query elements
+const input = document.getElementById("item-input");
+const addButton = document.getElementById("add-btn");
+const itemsUl = document.getElementById("shopping-list");
+const removeBtn = document.getElementById("remove-btn");
+
+
+console.log(input);
+console.log(addButton);
+console.log(itemsUl);
+console.log(removeBtn);
+
+function addAndDisplayItems() {
+   //add the item to the array(input line 54)
+    shoppingList.push(input.value);
+
+    //create a new element
+    let li = document.createElement('li');
+    
+    //update the element with the text
+    li.textContent = input.value;
+    
+    // append the new element to the ul
+    itemsUl.appendChild(li);
+    //clears the input text
+    input.value ="";
+    console.log(shoppingList);
+}
+
+
+//when click, add function
+function removeLastIteAndUpdate(){
+    //remove the last item in the array
+    shoppingList.pop();
+    //remove the last item from the ul
+    itemsUl.lastElementChild.remove();
+    console.log(shoppingList);
+}
+addButton.addEventListener('click', addAndDisplayItems);
+removeBtn.addEventListener('click', removeLastIteAndUpdate);
+
+
+// 3.Write another function that removes the last item and updates the displayed list when a “Remove Last Item” button is clicked.
