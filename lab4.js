b499ea5b01a6fabd4aebdba8b1f3909284ad22f6
eririn318git config --global user.email eririn75@hotmail.com <eririn75@hotmail.com>
2025-09-17 17:47:14 -0700

    added organized lab4.js

--- lab4.js
+++ lab4.js
@@ -14,8 +14,8 @@ function addItem(item){
 }
 //original
 //const addItem = (item)=>shoppingList.push(item);
-
 //const removeItem= ()=> shoppingList.pop();
+
 addItem('mouse')
 addItem('pc')
 addItem('monitor')
@@ -25,10 +25,10 @@ addItem('mac-pc')
 addItem('pcpc')
 addItem('putaropc')
 
+//removeItem()
 function removeItem(){
     shoppingList.pop()
 }
-//removeItem()
 
 function displayList(){
     console.log(shoppingList)
@@ -36,18 +36,17 @@ function displayList(){
 displayList()
 
 //Task2
-
 function filterItems(searchTerm){
     const result = shoppingList.filter((item) => item.includes(searchTerm))
     console.log(result);
 }
 console.log(shoppingList);
 filterItems('pc')
-//Task3
 
+//Task3
 // 1.Create an HTML page with an input field, an “Add Item” button, and an unordered list to display the items.
 // 2.Write a JavaScript function that adds items to the array and updates the displayed list dynamically when the button is clicked.
-//const shoppingList = [];
+// 3.Write another function that removes the last item and updates the displayed list when a “Remove Last Item” button is clicked.
 
 
 //select or query elements
@@ -56,14 +55,13 @@ const addButton = document.getElementById("add-btn");
 const itemsUl = document.getElementById("shopping-list");
 const removeBtn = document.getElementById("remove-btn");
 
-
 console.log(input);
 console.log(addButton);
 console.log(itemsUl);
 console.log(removeBtn);
 
 function addAndDisplayItems() {
-   //add the item to the array(input line 54)
+   //add the item to the array(input-line 53)
     shoppingList.push(input.value);
 
     //create a new element
@@ -74,11 +72,11 @@ function addAndDisplayItems() {
     
     // append the new element to the ul
     itemsUl.appendChild(li);
+
     //clears the input text
     input.value ="";
     console.log(shoppingList);
 }
-
 
 //when click, add function
 function removeLastIteAndUpdate(){
@@ -88,8 +86,8 @@ function removeLastIteAndUpdate(){
     itemsUl.lastElementChild.remove();
     console.log(shoppingList);
 }
+
 addButton.addEventListener('click', addAndDisplayItems);
 removeBtn.addEventListener('click', removeLastIteAndUpdate);
 
 
-// 3.Write another function that removes the last item and updates the displayed list when a “Remove Last Item” button is clicked.
